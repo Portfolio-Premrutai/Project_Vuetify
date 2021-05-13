3f1319693b8c522f09431f00cfe95189418e5afb
Sai <82368188+Portfolio-Premrutai@users.noreply.github.com>
2021-05-13 20:59:38 +0700

    demo1

---
+++ vuetify_demo/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-app>
+    <!-- div apply class -->
+    <v-app-bar app color="primary" dark>
+      <v-btn outline @click="$router.push('/')" target="_blank" text>Home</v-btn>
+
+      <v-btn outline @click="$router.push('/about')" target="_blank" text>
+        About
+        <!-- href="https://github.com/vuetifyjs/vuetify/releases/latest" -->
+      </v-btn>
+    </v-app-bar>
+    <!-- <div>
+      <v-toolbar dark prominent src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg">
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+        <v-toolbar-title>Vuetify</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn icon>
+          <v-icon>mdi-export</v-icon>
+        </v-btn>
+      </v-toolbar>
+    </div>-->
+
+    <v-content>
+      <router-view />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ vuetify_demo/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+<div id="home">
+  <v-container >
+   <h1>Home</h1> 
+   <!-- code color not working -->
+   <p class="green lighten-3 red--text text--darken-4">Premrutai</p>
+  </v-container>
+
+</div>
+</template>
+
+<script>
+export default {
+name:"home"
+}
+</script>
+
+<style>
+
+</style>
